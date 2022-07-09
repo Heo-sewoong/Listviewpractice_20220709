9a4efdc0255fde46c298c67fcfe18d8ff68946c0
허세웅 <science177@daum.net>
2022-07-09 12:52:45 +0900

    5. 어댑터 객체화(리스트 전달) / ListView 태그와 연결 - 실데티어 반영x

--- app/src/main/java/com/example/listviewpractice_20220709/adatpters/StudentListAdapter.kt
+++ app/src/main/java/com/example/listviewpractice_20220709/adatpters/StudentListAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.example.listviewpractice_20220709.R
 import com.example.listviewpractice_20220709.StudentData
 import java.math.MathContext
@@ -23,6 +24,17 @@ class StudentListAdapter(
 
         }
         val row = tempRow!!
+
+        val nameTxt = row.findViewById<TextView>(R.id.nameTxt)
+        val ageTxt = row.findViewById<TextView>(R.id.ageTxT)
+        val addressTxt = row.findViewById<TextView>(R.id.addressTxt)
+
+        // mList[position] ==> mList의 position번째 있는 studentData
+
+        nameTxt.text = mList[position].name
+// 도전과제 => String을 가공(${} 활용) => 나이부분의 값이 (~~세)로 표시되도록 가공
+        ageTxt.text = mList[position].birthYear.toString()
+        addressTxt.text = mList[position].address
 
         return row
     }
