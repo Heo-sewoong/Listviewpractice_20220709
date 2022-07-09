2b9fda0816e73753104325426e712dc6cb3d20e1
허세웅 <science177@daum.net>
2022-07-09 12:39:42 +0900

    5. 어댑터 객체화(리스트 전달) / ListView 태그와 연결 - 실데티어 반영x

--- app/src/main/java/com/example/listviewpractice_20220709/MainActivity.kt
+++ app/src/main/java/com/example/listviewpractice_20220709/MainActivity.kt
@@ -2,9 +2,16 @@ package com.example.listviewpractice_20220709
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.listviewpractice_20220709.adatpters.StudentListAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     val mStudentList = ArrayList<StudentData>()
+
+
+    lateinit var mstudentListAdapter: StudentListAdapter
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,6 +29,14 @@ class MainActivity : AppCompatActivity() {
         mStudentList.add(StudentData("김민철9",1990,"서울시 서대문구"))
         mStudentList.add(StudentData("김민철10",1990,"서울시 서대문구"))
         // 학생 데이터(더미 데이터 생성)
+
+        // 실제 어댑터와 Arraylist를 연결(어댑터 클래스르
+
+        mstudentListAdapter = StudentListAdapter(mContext = this, R.layout.student_list_item, mStudentList)
+
+        // 액티비티 화면의 ListView 태그와 adpater를 연결
+
+        mainListView.adapter = mstudentListAdapter
 
 
     }
--- app/src/main/java/com/example/listviewpractice_20220709/adatpters/StudentListAdapter.kt
+++ app/src/main/java/com/example/listviewpractice_20220709/adatpters/StudentListAdapter.kt
@@ -1,7 +1,11 @@
 package com.example.listviewpractice_20220709.adatpters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.example.listviewpractice_20220709.R
 import com.example.listviewpractice_20220709.StudentData
 import java.math.MathContext
 
@@ -11,4 +15,15 @@ class StudentListAdapter(
     val mList: ArrayList<StudentData>
 
     ) : ArrayAdapter<StudentData>(mContext, resID, mList) {
+    // 한 칸의 xml에 나타날 모양을 커스터마이징
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.student_list_item,null)
+
+        }
+        val row = tempRow!!
+
+        return row
+    }
 }
